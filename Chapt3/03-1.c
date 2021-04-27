121d3546e51c1eb6600e0e5e57165eac9b420023
hyojin-park24 <gygy332@naver.com>
2021-04-27 16:16:07 +0900

    초기 커밋.

---
+++ Chapt3/03-1.c
@@ -0,0 +1,31 @@
+//// 변수 선언과 사용
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a; //int형 변수 a선언
+//	int b, c; //2개의 int 형 변수 b,c를 동시 선언
+//	double da; // double형 변수 da 선언
+//	char ch; // char형 변수  ch 선언
+//
+//	a = 10; // int 형 변수 a에 정수 10 대입 
+//	b = a; // int형 변수 b에 변수 a의 값 대입 
+//	c = a + 20;
+//	da = 3.5;
+//	ch = 'A'; 
+//
+//	printf("변수 a의 값 : %d\n", a);
+//	printf("변수 b의 값 : %d\n", b);
+//	printf("변수 c의 값 : %d\n", c);
+//	printf("변수 da의 값 : %.1lf\n", da);
+//	printf("변수 ch의 값 : %c\n", ch);
+//
+//	printf("char 크기 : %d\n", sizeof(char));
+//	printf("int 크기 : %d\n", sizeof(int));
+//	printf("double 크기 : %d\n", sizeof(double));
+//	printf("short 크기 : %d\n", sizeof(short));
+//	printf("long 크기 : %d\n", sizeof(long));
+//
+//	return 0;
+//}
---
+++ Chapt3/03-7.c
@@ -0,0 +1,15 @@
+//// char 배열에 문자 복사 strcpy 
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char fruit[20] = "strawberry"; //strawberry 초기화
+//
+//	printf("%s\n", fruit);        //strawberry 출력
+//	strcpy(fruit, "banana");      // fruit에 banana 복사
+//	printf("%s\n", fruit);        // banana 출력
+//
+//	return 0;
+//}
