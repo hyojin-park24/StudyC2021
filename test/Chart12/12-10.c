1623c25088242f38538f3743e925168d2bad185e
hyojin-park24 <gygy332@naver.com>
2021-05-04 17:31:03 +0900

    배열과 포인터/문자/문자열

---
+++ test/Chart12/12-10.c
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char str[80] = "straw";
+//
+//	strcat(str, "berry");		// str 배열에 문자열 붙이기
+//	printf("%s\n", str);
+//	strncat(str, "piece", 3);	// str 배열에 3개의 문자 붙이기
+//	printf("%s\n", str);
+//
+//	return 0;
+//}
---
+++ test/Chart12/12-12.c
@@ -0,0 +1,27 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char str1[80] = "pear";
+//	char str2[80] = "peach";
+//
+//	// strcmp 함수
+//	//printf("사전에 나중에 나오는 과일 이름 : ");
+//
+//	//if (strcmp(str1, str2) > 0)		//str1이 str2보다 크면(사전에 나중에 나오면)
+//	//	printf("%s\n", str1);		// str1 출력 : 아스키 코드값이 더 큼
+//	//else							// str1이 str2보다 크지 않으면
+//	//	printf("%s\n", str2);		// str2 출력
+//
+//	// strncmp 함수
+//	printf("앞에서 3개의 문자만 비교하면?\n");
+//
+//	if (strncmp(str1, str2, 3) == 0)
+//		printf("같다.\n");
+//	else
+//		printf("다르다.\n");
+//
+//	return 0;
+//}
