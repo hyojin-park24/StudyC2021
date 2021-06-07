c13a56c189654d0602de3be886b2530d36ad1d40
hyojin-park24 <gygy332@naver.com>
2021-06-07 17:21:13 +0900

    ㅎㅎ모르겠다~

--- Evalution/fitnessCareProgram.c
+++ Evalution/fitnessCareProgram.c
@@ -7,6 +7,7 @@
 */
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct {
@@ -15,13 +16,41 @@ typedef struct {
 	double weight;
 }Fitness;
 Fitness* ary[100];
-void total_number(int count);
-double average_weight(Fitness** pary, int count);
-int max_weight(Fitness** pary, int count);
-void print_info(Fitness** pary, int index);
-void free_ary(Fitness** pary, int count);
+void total_number(int count);						//총회원 수 함수
+double average_weight(Fitness** pary, int count);	//평균 체중 함수
+int max_weight(Fitness** pary, int count);			//최고 체중 함수
+void print_info(Fitness** pary, int index);			//출력 함수
+void free_ary(Fitness** pary, int count);			//동적할당 반환 함수
 int main()
 {
+	int i;
+	int num = 0;		//회원번호
+	char name[20];
+	
+	ary[i] = malloc(sizeof(Fitness) * num);
+	if (ary == NULL) { printf("메모리 부족"); exit(1); }
+	for (int i = 0; i < num; i++)
+	{
+		scanf("%d %s %f", &ary[i].num, name, &ary[i].weight);
+		(ary +i)->name = (char*)malloc(strlen(name) + 1);		//
+		if ((ary + i)->name == NULL) { printf("메모리 부족"); exit(1); }
+		strcpy((ary + 1)->name, name);
+	}
+	for (int i = 0; i < num; i++)
+	{
+		printf("회원 번호 : %d", (people + i)->num);
+		printf("이름 입력 : %s", (people + i)->name);
+		printf("체중 입력 : %f", (people + i)->weight);
+	}
 
 	return 0;
 }
+
+void total_number(int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		count++;
+	}
+}
+
